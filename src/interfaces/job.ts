892964683ce797967990c18c8748fd61ec490584
Francisco Miamoto <fsmiamoto@gmail.com>
2020-02-22 23:42:43 -0300

    feat: add interfaces for data types

---
+++ src/interfaces/job.ts
@@ -0,0 +1,16 @@
+export interface Job {
+  code: string;
+  description: string;
+  jobType: string;
+  date: string;
+  majors: string[];
+  company: string;
+  requisites: string;
+  workHours: string;
+  salary: string;
+  benefits: string;
+  positions: string;
+  contact: string;
+  phone: string;
+  email: string;
+}
---
+++ src/interfaces/pageInfo.ts
@@ -0,0 +1,8 @@
+export interface PageInfo {
+  perPage: number;
+  total: number;
+  currentPage: number;
+  lastPage: number;
+  hasNextPage: boolean;
+  hasPreviousPage: boolean;
+}
