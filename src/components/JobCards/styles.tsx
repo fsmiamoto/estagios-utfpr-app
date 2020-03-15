c847a3598bc08b2afcd6b8c4b235dc6c4683cac4
Francisco Miamoto <fsmiamoto@gmail.com>
2020-03-14 22:19:52 -0300

    feat: change flex to grid for JobCards
    Grid is the correct choice for the
    functionality  and now it has changed to it

--- src/components/JobCards/styles.tsx
+++ src/components/JobCards/styles.tsx
@@ -1,10 +1,20 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
-  flex-flow: wrap;
-  flex: 1 1 100%;
-  justify-content: center;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
   height: 100%;
   z-index: 100;
+
+  @media (max-width: 576px) {
+    grid-template-columns: 1fr;
+  }
+
+  @media (min-width: 992px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (min-width: 1600px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
 `;
