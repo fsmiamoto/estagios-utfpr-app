3bbb0427d9c6f8ace33dac29e59d66549e51a313
Francisco Miamoto <fsmiamoto@gmail.com>
2020-03-14 22:25:43 -0300

    feat: add pageIndicator component

---
+++ src/components/PageIndicator/index.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export default styled.h3`
+  margin-right: 20px;
+  color: #777;
+  font-weight: normal;
+  font-size: 0.95rem;
+  text-align: end;
+  margin-bottom: 10px;
+`;
