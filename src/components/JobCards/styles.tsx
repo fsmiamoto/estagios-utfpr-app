0527053315ddf65eb84c5834d996aa539d36f999
Francisco Miamoto <fsmiamoto@gmail.com>
2020-06-28 18:11:17 -0300

    fix: use minmax func for card width

--- src/components/JobCards/styles.tsx
+++ src/components/JobCards/styles.tsx
@@ -2,19 +2,19 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(2, minmax(270px,1fr));
   height: 100%;
   z-index: 100;
 
   @media (max-width: 576px) {
-    grid-template-columns: 1fr;
+    grid-template-columns: minmax(270px,1fr);
   }
 
   @media (min-width: 992px) {
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(2, minmax(270px,1fr));
   }
 
   @media (min-width: 1600px) {
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(3, minmax(270px,1fr));
   }
 `;
